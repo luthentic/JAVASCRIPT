eb33f267fb4e4fe9a780994c8983e93b5766cffe
Lee <dltpdnd1234@gmail.com>
2021-05-09 22:13:20 -0500

    Js review basic stuffs

--- Basic/basicPractice.js
+++ Basic/basicPractice.js
@@ -53,3 +53,78 @@ const concate = array.concat(array2,array3);
 console.log(concate.slice(2,8));
 // concatenate array
 
+const points = [40, 100, 1, 5, 25, 10];
+console.log(points.sort((a,b)=>{return a - b})); 
+console.log(points.sort(function(a, b){return b - a}));
+//numeric sort
+
+let i,j,k;
+for(i=points.length-1; i>0; i--){
+  j = Math.floor(Math.random() * i);
+  k = points[i];
+  points[i] = points[j];
+  points[j] = k;
+}
+console.log(points);
+
+console.log(points.sort(function(a, b){return 0.5 - Math.random()})); 
+//random Sorting an Array in Random Order
+//The Math.random() function returns a floating-point, pseudo-random number in the range 0 to less than 1
+//Math.floor(x) returns the value of x rounded down to its nearest integer:
+
+let txt = "";
+let number = [10,20,30,40,50];
+number.forEach( value =>{
+  txt = `${txt}  ${value}`;
+  console.log(txt);
+});
+//for each iteration
+
+let txt1 = "";
+number.map( value =>{
+  txt1 = `${txt1}  ${value}`;
+  console.log(txt1);
+});
+// The map() method creates a new array by performing a function on each array element.
+// The map() method does not execute the function for array elements without values.
+// The map() method does not change the original array.
+//map iteration
+
+const sum = number.reduceRight(myfunction, 23);
+function myfunction(total,value){
+  return total + value;
+}
+console.log(sum);
+// reduceright iteration
+
+const every = number.every(value=>{
+  return value > 10;
+});
+console.log(every);
+// The every() method check if all array values pass a test.
+
+const some = number.some(value=>{
+  return value >10;
+});
+console.log(some);
+//The some() method check if some array values pass a test.
+
+const date = new Date();
+console.log(date.getFullYear());
+date.setFullYear(2021,1,03); // mongth: 0 = Jan, 1 = Feb
+console.log(date);
+// Date
+
+console.log(`Math.ceil`,Math.ceil(4.1));
+console.log(`Math.floor`,Math.floor(4.9));
+console.log(`Math.trunc`,Math.trunc(4.9));
+console.log(`Math.sign`,Math.sign(-4.9));
+console.log(`Math.sign`,Math.sign(4.9));
+console.log(`Math.sqrt`,Math.sqrt(4.9));
+console.log(`Math.abs`,Math.abs(-4.9));
+console.log(Math.min(1,2,3,4,5,6,7));
+console.log(Math.max(1,2,3,4,5,6,7));
+console.log(Math.floor(Math.random() * 100) + 1);
+// Math
+
+
