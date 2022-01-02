683773a339fabab2432b67ecaf84297c56b99634
DESKTOP-HHHASMJ\dltpd <dltpdnd1234@gmail.com>
2022-01-01 22:59:15 -0600

    rest operator

--- Basic/object.js
+++ Basic/object.js
@@ -48,3 +48,29 @@ let id2 = Symbol("id");
 console.log(id1.description);
 
 console.log(id1 === id2);
+
+
+function myFun(a, b, ...restOb){
+  console.log("a", a);
+  console.log("b", b);
+  console.log("rest", restOb);
+}
+
+myFun("One", "Two", "Three", "Four", "Five");
+
+
+function fucLen(a, b, ...arg){
+  console.log(arg.length);
+}
+
+fucLen(1,2,3,4,5,6,7,8);
+
+
+function multifly(multi, ...arg){
+  return arg.map((element)=>{
+    return multi * element;
+  });
+}
+
+let mulit = multifly(2, 15, 20, 30, 40);
+console.log(mulit);
